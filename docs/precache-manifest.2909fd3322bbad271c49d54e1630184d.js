461a6f3176facd094c456dc02dd0137c3cf858b6
Sha Kong-Brooks <sha@rocketlab.com.au>
2019-12-19 10:31:44 +1100

    new build

---
+++ docs/precache-manifest.2909fd3322bbad271c49d54e1630184d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431f2eb163f9c386de48c28c6b3f3bf2",
+    "url": "/scandit/index.html"
+  },
+  {
+    "revision": "c17b989be0d6489b739f",
+    "url": "/scandit/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6aa3c01c5ce4cea5f922",
+    "url": "/scandit/static/js/2.2fc340ce.chunk.js"
+  },
+  {
+    "revision": "c327df52f63686aaa0caaec0921a1ed0",
+    "url": "/scandit/static/js/2.2fc340ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c17b989be0d6489b739f",
+    "url": "/scandit/static/js/main.11cfe1c6.chunk.js"
+  },
+  {
+    "revision": "b7fa1474e28ce566da49",
+    "url": "/scandit/static/js/runtime-main.128045ff.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91b0ce2200edcd8d1d5b436888eb1f19.js"
+  "/scandit/precache-manifest.2909fd3322bbad271c49d54e1630184d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scandit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
